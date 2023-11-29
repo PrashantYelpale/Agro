645c0c06621e0d31ccd91b96447e2436d43abbc5
Prashant Yelpale <144728514+PrashantYelpale@users.noreply.github.com>
2023-11-29 19:23:53 +0530

    Add files via upload

---
+++ reviewInput.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    require 'db.php';
+
+    $rating = $_POST['rating'];
+    $review = $_POST['comment'];
+    $name = $_SESSION['Name'];
+    $pid = $_GET['pid'];
+
+    // echo $rating.$review.$pid.$name;
+    $sql = "INSERT INTO review(pid,name,rating,comment)
+            VALUES('$pid','$name', '$rating', '$review')";
+
+    $result = mysqli_query($conn, $sql);
+    if(!$result)
+    {
+        echo mysqli_error($conn);
+
+    }
+    else {
+        header("Location: review.php?pid=".$pid);
+    }
+
+?>
